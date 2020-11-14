ffd833aaa1b57930deb0779ae45587e00fdb156f
Sayan Bhattacharjee <sayanb06@users.noreply.github.com>
2020-11-14 04:47:09 -0800

    Updated get_account to set last login time

--- Database/get_account.sql
+++ Database/get_account.sql
@@ -1,7 +1,12 @@
 DELIMITER //
 CREATE OR REPLACE PROCEDURE GET_ACCOUNT(user_name VARCHAR(256), potentialHashedPassword CHAR(128)) 
 BEGIN 
-SELECT * FROM (SELECT * FROM Account WHERE Username=user_name AND HashedPassword=potentialHashedPassword LIMIT 1) as acctRow NATURAL JOIN User;
+UPDATE Account SET LatestLoginTime=CURRENT_TIME WHERE Username=user_name AND HashedPassword=potentialHashedPassword;
+SELECT * FROM (SELECT * 
+               FROM Account 
+               WHERE Username=user_name AND HashedPassword=potentialHashedPassword LIMIT 1)
+               as acctRow NATURAL JOIN User;
 END;
 //
 DELIMITER ;
+                                                                                             
