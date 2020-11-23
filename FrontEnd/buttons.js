e59ee0fc44fef6b51208e42e5c68b5b7557e4e6c
reetahan <eunsunl@illinois.edu>
2020-11-23 05:37:10 -0600

    adding back files previously removed

--- FrontEnd/buttons.js
+++ FrontEnd/buttons.js
@@ -1,1 +1,14 @@
-../ears/FrontEnd/buttons.js
+<div class="container">	
+  <h2>Justified Button Groups</h2>	
+  <p>To span the entire width of the screen, use the .btn-group-justified class:</p>	
+  <div class="btn-group btn-group-justified">	
+    <a href="#" class="btn btn-primary">Display Entire Table</a>	
+    <a href="#" id="search" class="btn btn-primary">Search</a>	
+    $('#search').on('click', function (e) {	
+        Hello	
+    })	
+    <a href="#" class="btn btn-primary">Insert</a>	
+    <a href="#" class="btn btn-primary">Delete</a>	
+    <a href="#" class="btn btn-primary">Update</a>	
+  </div>	
+</div>
