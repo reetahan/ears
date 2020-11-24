70aea6fd6e87027e705065a657d52661855e6cab
Sayan Bhattacharjee <sayanb06@gmail.com>
2020-11-23 20:12:04 -0600

    Added Hashing Capabilities

--- Database/get_account.sql
+++ Database/get_account.sql
@@ -1,12 +1,12 @@
 DELIMITER //
-CREATE OR REPLACE PROCEDURE GET_ACCOUNT(user_name VARCHAR(256), potentialHashedPassword CHAR(128)) 
+CREATE OR REPLACE PROCEDURE GET_ACCOUNT(user_name VARCHAR(256)) 
 BEGIN 
-UPDATE Account SET LatestLoginTime=CURRENT_TIME WHERE Username=user_name AND HashedPassword=potentialHashedPassword;
+UPDATE Account SET LatestLoginTime=CURRENT_TIME WHERE Username=user_name;
 SELECT * FROM (SELECT * 
                FROM Account 
-               WHERE Username=user_name AND HashedPassword=potentialHashedPassword LIMIT 1)
+               WHERE Username=user_name 
+               LIMIT 1)
                as acctRow NATURAL JOIN User;
 END;
 //
 DELIMITER ;
-                                                                                             
