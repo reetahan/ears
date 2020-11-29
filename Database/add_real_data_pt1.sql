33e03b0791ebfcf05009c3bd3b51d883a757cc02
reetahan <eunsunl@illinois.edu>
2020-11-29 16:56:39 -0600

    adding data

---
+++ Database/add_real_data_pt1.sql
@@ -0,0 +1,114 @@
+/*
+CALL USER_INSERT("Soomin Nam","Soomin","$2b$13$Z849aTpIHZhb4BEN6qKum.LmSpJzm5b.eVtOLtur56yNHY7q7C3HK") ;
+CALL COURSE_INSERT("MCB450","Carnevale","12-01-2020 11:00",14) ;
+CALL COURSE_INSERT("CHEM442","Backlund","11-30-2020 10:00",14) ;
+CALL COURSE_INSERT("CHBE421","Harley","12-01-2020 09:30",14) ;
+CALL COURSE_INSERT("CHEM222","Harris","11-30-2020 13:00",14) ;
+CALL COURSE_INSERT("MUS133","Broussard","12-01-2020 12:00",14) ;
+CALL EVENT_INSERT("HW 7",14,"11/25/20 23:59","Penultimate homework",197) ;
+CALL EVENT_INSERT("HW 8",14,"12/10/20 23:59","Final homework",197) ;
+CALL COURSELINK_INSERT(197,"https://access5.lon-capa.illinois.edu/adm/navmaps?postdata=%2fadm%2femail&postsymb=","Lon Capa") ;
+CALL EVENT_INSERT("HW 9",14,"11/30/20 23:59","Penultimate homework",198) ;
+CALL EVENT_INSERT("Final Exam",14,"12/14/20 12:00","Cumulative exam",198) ;
+CALL COURSELINK_INSERT(198,"https://compass2g.illinois.edu/webapps/blackboard/content/listContent.jsp?course_id=_53087_1&content_id=_4635162_1","Compass") ;
+CALL EVENT_INSERT("HW 9",14,"11/19/20 23:59","Dynamics homework",199) ;
+CALL EVENT_INSERT("HW 10",14,"12/01/20 23:59","Final homework",199) ;
+CALL COURSELINK_INSERT(199,"https://compass2g.illinois.edu/webapps/blackboard/content/listContent.jsp?course_id=_53052_1&content_id=_4631878_1","Compass") ;
+CALL EVENT_INSERT("Exam 3",14,"11/04/20 19:00","Third midterm, ch 7-9",200) ;
+CALL EVENT_INSERT("HW 11-12",14,"11/15/20 23:59","Combined homework",200) ;
+CALL COURSELINK_INSERT(200,"https://compass2g.illinois.edu/webapps/blackboard/content/listContent.jsp?course_id=_50904_1&content_id=_4449568_1","Compass") ;
+CALL EVENT_INSERT("DB 3",14,"11/18/20 23:59","General debate question of your choice",201) ;
+CALL EVENT_INSERT("Writing Assignment",14,"11/18/20 23:59","Peruvian instrument write-up",201) ;
+CALL COURSELINK_INSERT(201,"https://compass2g.illinois.edu/webapps/blackboard/execute/announcement?method=search&context=course_entry&course_id=_53528_1&handle=announcements_entry&mode=view","Compass") ;
+
+CALL USER_INSERT("Sita Jothishankar","Sita","$2b$13$rDx8KToDi/.euF6EYHxQp.0rJ4IqMhCMf/mY7NUOzueh61pRBPiY2") ;
+CALL COURSE_INSERT("CS410","Zhai","11-30-2020 13:00",17) ;
+CALL COURSE_INSERT("CS498","Delgosha","12-01-2020 11:00",17) ;
+CALL COURSE_INSERT("ECE316","Hilmer","12-01-2020 14:00",17) ;
+CALL COURSE_INSERT("SPED117","Bentz","12-02-2020 17:00",17) ;
+CALL COURSE_INSERT("CS465","Bailey","11-30-2020 09:30",17) ;
+CALL EVENT_INSERT("Quiz 7",17,"2020-11-22 23:59","Due at start of fall break",212) ;
+CALL EVENT_INSERT("Quiz 8",17,"2020-11-22 23:59","Due at start of fall break",212) ;
+CALL EVENT_INSERT("Quiz 9",17,"2020-11-22 23:59","Due at start of fall break",212) ;
+CALL EVENT_INSERT("Quiz 10",17,"2020-11-22 23:59","Due at start of fall break",212) ;
+CALL EVENT_INSERT("Quiz 11",17,"2020-11-22 23:59","Due at start of fall break",212) ;
+CALL EVENT_INSERT("Quiz 12",17,"2020-11-22 23:59","Due at start of fall break",212) ;
+CALL COURSELINK_INSERT(212,"https://coursera.com","Coursera") ;
+CALL EVENT_INSERT("MP2",17,"2020-12-09 23:59","Due at end of semester",213) ;
+CALL EVENT_INSERT("MP3",17,"2020-12-09 23:59","Due at end of semester",213) ;
+CALL EVENT_INSERT("MP4",17,"2020-12-09 23:59","Due at end of semester",213) ;
+CALL EVENT_INSERT("MP5",17,"2020-12-09 23:59","Due at end of semester",213) ;
+CALL EVENT_INSERT("MP6",17,"2020-12-09 23:59","Due at end of semester",213) ;
+CALL EVENT_INSERT("MP7",17,"2020-12-09 23:59","Due at end of semester",213) ;
+CALL EVENT_INSERT("MP8",17,"2020-12-09 23:59","Due at end of semester",213) ;
+CALL EVENT_INSERT("MP9",17,"2020-12-09 23:59","Due at end of semester",213) ;
+CALL EVENT_INSERT("MP10",17,"2020-12-09 23:59","Due at end of semester",213) ;
+CALL EVENT_INSERT("MP11",17,"2020-12-09 23:59","Due at end of semester",213) ;
+CALL EVENT_INSERT("MP12",17,"2020-12-09 23:59","Due at end of semester",213) ;
+CALL COURSELINK_INSERT(213,"https://coursera.com","Coursera") ;
+CALL EVENT_INSERT("Position Paper 2",17,"2020-11-08 22:59","Data privacy and ethics paper",214) ;
+CALL EVENT_INSERT("Research Paper",17,"2020-11-20 22:59","My choice",214) ;
+CALL COURSELINK_INSERT(214,"https://compass2g.illinois.edu","Compass") ;
+CALL EVENT_INSERT("Issue Brief",17,"2020-09-20 22:59","Group assignmnent",215) ;
+CALL EVENT_INSERT("Final Paper",17,"2020-10-14 22:59","Disability culture paper",215) ;
+CALL COURSELINK_INSERT(215,"https://moodle.illinois.edu","Moodle") ;
+CALL EVENT_INSERT("HW2",17,"2020-10-07 23:59","UX/UI",216) ;
+CALL EVENT_INSERT("Hw3",17,"2020-11-11 23:59","Space design homework",216) ;
+CALL COURSELINK_INSERT(216,"https://compass2g.illinois.edu","Compass") ;
+
+
+CALL USER_INSERT("Eunsun Lee","eunsunl2","$2b$13$DKK2nSDJvM6W4PKzONK3zuDgXLwLktmANU9PrcKlg8yDCy3iH95hG") ;
+CALL COURSE_INSERT("CS465","Bailey","11-30-2020 09:30",19) ;
+CALL COURSE_INSERT("CS446","Zhai","11-30-2020 13:00",19) ;
+CALL COURSE_INSERT("CS411","Alawini","11-30-2020 11:00",19) ;
+CALL EVENT_INSERT("HW3",19,"2020-11-11 23:59","Space design homework",220) ;
+CALL COURSELINK_INSERT(220,"https://compass2g.illinois.edu","Compass") ;
+CALL EVENT_INSERT("HW7",19,"2020-11-20 23:59","Query Processing homework",221) ;
+CALL EVENT_INSERT("Exam 3",19,"2020-12-08 19:00","Chapter 14-18 exam",221) ;
+CALL EVENT_INSERT("Semester Project",19,"2020-12-02 14:40","EARS Project",221) ;
+CALL COURSELINK_INSERT(221,"https://compass2g.illinois.edu","Compass") ;
+CALL COURSELINK_INSERT(221,"https://campuswire.com","Campuswire") ;
+CALL COURSELINK_INSERT(221,"https://gradescope.com","Gradescope") ;
+CALL EVENT_INSERT("HW5",19,"2020-12-02 23:59","Unsupervised Learning",222) ;
+CALL EVENT_INSERT("Semester Project",19,"2020-12-09 22:59","Paper review",222) ;
+CALL COURSELINK_INSERT(222,"https://piazza.com","Piazza") ;
+
+
+CALL USER_INSERT("Danbie Yoon","danbiey2","$2b$13$OjAJmMkkMxIK.OxVOhJf8uqUj95Lf6JX1.AVi1tk67APEfIDWr90G") ;
+CALL COURSE_INSERT("LER565","Won","12-01-2020 14:00",20) ;
+CALL COURSE_INSERT("LER523","Benton","12-02-2020 08:00",20) ;
+CALL COURSE_INSERT("LER591","Cardador","12-01-2020 08:00",20) ;
+CALL COURSELINK_INSERT(223,"https://compass2g.illinois.edu/webapps/blackboard/execute/announcement?method=search&context=course_entry&course_id=_53722_1&handle=announcements_entry&mode=view","Compass") ;
+CALL COURSELINK_INSERT(224,"https://compass2g.illinois.edu/webapps/blackboard/content/listContent.jsp?course_id=_50921_1&content_id=_4451294_1","Compass") ;
+CALL COURSELINK_INSERT(225,"https://compass2g.illinois.edu/webapps/blackboard/content/listContent.jsp?course_id=_51024_1&content_id=_4457750_1","Compass") ;
+
+
+CALL USER_INSERT("Amanda Wang","bigchungus","$2b$13$owMKfMm.EI0qOd1oIUz5q.dD9bMXpuEHgKkbg8rFkmDxMmxdBmrEW") ;
+CALL COURSE_INSERT("CS374","Har-Peled","12-01-2020 11:00",21) ;
+CALL COURSE_INSERT("SPED117","Bentz","12-02-2020 17:00",21) ;
+CALL COURSE_INSERT("ECON102","Vazquez","11-30-2020 13:00",21) ;
+CALL EVENT_INSERT("HW9",21,"2020-11-04 09:59","NP-Hardness",226) ;
+CALL EVENT_INSERT("HW10",21,"2020-11-18 09:59","Decidability",226) ;
+CALL COURSELINK_INSERT(226,"https://learn.illinois.edu/course/view.php?id=50851","Moodle") ;
+CALL EVENT_INSERT("Issue Brief",21,"2020-09-20 22:59","Group assignmnent",227) ;
+CALL EVENT_INSERT("Final Paper",21,"2020-10-14 22:59","Disability culture paper",227) ;
+CALL COURSELINK_INSERT(227,"https://learn.illinois.edu/course/view.php?id=45984","Moodle") ;
+CALL EVENT_INSERT("Quiz 10",21,"2020-12-09 23:59","Game Theory",228) ;
+CALL COURSELINK_INSERT(228,"https://learn.illinois.edu/course/view.php?id=36115","Moodle") ;
+
+CALL USER_INSERT("Yee Chan Kim","yck2","$2b$13$XPFPVxEGikOk87GIUoknIu48p09mXPur1xIsZfpRH.N2g4gyXRis2") ;
+CALL COURSE_INSERT("ECE304","Choquette","11-30-2020 10:00",22) ;
+CALL COURSE_INSERT("ECE385","Cheng","11-30-2020 16:00",22) ;
+CALL COURSE_INSERT("GEOL107","Lundstrom","11-30-2020 11:00",22) ;
+CALL COURSE_INSERT("GEOL117","Peng","12-01-2020 12:00",22) ;
+CALL EVENT_INSERT("HW8",22,"2020-11-30 09:59","Photonic devices",229) ;
+CALL COURSELINK_INSERT(229,"https://wiki.illinois.edu/wiki/spaces/viewspace.action?key=ECE304FA20","Class Site") ;
+CALL EVENT_INSERT("Final Project Report",22,"2020-12-16 23:59","Typed report for semeseter project",230) ;
+CALL COURSELINK_INSERT(230,"https://wiki.illinois.edu/wiki/pages/viewpage.action?spaceKey=ECE385FA20&title=Home","Class Site") ;
+CALL EVENT_INSERT("Pre-lecture Quiz 39",22,"2020-11-30 10:59","pre lecture before class",231) ;
+CALL COURSELINK_INSERT(231,"https://learn.illinois.edu/course/view.php?id=49884","Moodle") ;
+CALL EVENT_INSERT("Lesson 12",22,"2020-11-20 10:59","complete lesson material by break",232) ;
+CALL COURSELINK_INSERT(232,"https://learn.illinois.edu/course/view.php?id=53430","Moodle") ;
+
+*/
+
