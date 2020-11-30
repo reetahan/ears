208878e895d331bed1e94079c8372149d61aa508
reetahan <eunsunl@illinois.edu>
2020-11-30 03:07:48 -0600

    completed signup page, added logout button, moved files from server to git repo

---
+++ FrontEnd/signup.js
@@ -0,0 +1,30 @@
+const url = "http://earsapp411.web.illinois.edu/api";
+
+function sendPostRequest(params) {
+  opts = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(params),
+  };
+  fetch(url, opts)
+    .then((response) => response.text())
+    .then(
+      (data) => (document.getElementById("signup_status").textContent = data)
+    );
+}
+
+function attemptSignup() {
+  let inputFullName = document.getElementById("fullname").value;
+  let inputUsername = document.getElementById("username").value;
+  let inputPassword = document.getElementById("password").value;
+  let params = {
+    apiType: "User",
+    action: "Insert",
+    fullName: inputFullName,
+    userName: inputUsername,
+    plaintextPassword: inputPassword,
+  };
+  sendPostRequest(params);
+}
